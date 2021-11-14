ba732ceab39582288e70d28af5ce96aa36bea3ad
Febri Damatraseta <febri.damatraseta@sgu.ac.id>
2021-11-14 19:55:58 +0700

    update-main2

--- src/reguler/Pertemuan8-review-materi/Latihan-Retake-UTS/Main2.java
+++ src/reguler/Pertemuan8-review-materi/Latihan-Retake-UTS/Main2.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Main2 {
     
-    Person p = new Person(); 
     public List <Person> ListPerson = new ArrayList<Person>();
 
     public static void main(String[] args) {
@@ -17,36 +16,39 @@ public class Main2 {
             System.out.println("Data ke-"+i);    
             System.out.println("===============");
             m.Inputform();
-            m.ListPerson.add(m.p);
         }
         
-        m.Outputform();    
+        m.Outputform();
     }
 
     public void Inputform(){
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Form isian data");
-        
-        System.out.print("Masukan name :");
-        String Name = myObj.nextLine();
-        p.setName(Name);
+            Person p = new Person();
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Form isian data");
+            
+            System.out.print("Masukan name :");
+            String Name = myObj.nextLine();
+            p.setName(Name);
 
-        System.out.print("Masukan Gender :");
-        String Gender = myObj.nextLine();
-        p.setGender(Gender.charAt(0));
+            System.out.print("Masukan Gender :");
+            String Gender = myObj.nextLine();
+            p.setGender(Gender.charAt(0));
 
-        
-        System.out.print("Masukan Birthdate :");
-        String Birthdate = myObj.nextLine();
-        p.setBirthdate(Birthdate);
+            
+            System.out.print("Masukan Birthdate :");
+            String Birthdate = myObj.nextLine();
+            p.setBirthdate(Birthdate);
 
-        System.out.print("Masukan Height :");
-        String Height = myObj.nextLine();
-        p.setHeight( Integer.parseInt(Height));
+            System.out.print("Masukan Height :");
+            String Height = myObj.nextLine();
+            p.setHeight( Integer.parseInt(Height));
 
-        System.out.print("Masukan Weight :");
-        String Weight = myObj.nextLine();
-        p.setWeight( Integer.parseInt(Weight));
+            System.out.print("Masukan Weight :");
+            String Weight = myObj.nextLine();
+            p.setWeight( Integer.parseInt(Weight));
+            
+            //add to array list
+            ListPerson.add(p);
     }
     
     public void Outputform(){
@@ -57,8 +59,6 @@ public class Main2 {
             System.out.println("=================="); 
             System.out.println("Name : "+p.getName());
             String type = "";
-
-
             if (p.getGender() == 'p' || p.getGender() == 'P') {
                 type = "Perempuan";
             } else if(p.getGender() == 'l' || p.getGender() == 'L') {
